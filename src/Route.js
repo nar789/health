0b05b315a1142b8a6a1caa4671ea288db6a95a17
nar789 <admin@hume.co.kr>
2023-10-13 07:23:55 +0900

    init

---
+++ src/Route.js
@@ -0,0 +1,22 @@
+(function () {
+  module.exports = function (_g) {
+    var app = _g.app;
+    var http = _g.http;
+
+    function route() {
+      app.get("/", function (req, res) {
+        res.render("index.html", {});
+      });
+
+      //1. enetry point
+      app.listen(1003, function () {
+        console.log("health Server listen on *:1003");
+      });
+    }
+
+    var publicReturn = {
+      route: route,
+    };
+    return publicReturn;
+  };
+})();
